d533e94ab44dbbe9c3dbae496dcd023f275d122a
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2017-08-03 18:03:30 +0200

    Renamed existing python modules to more closely reflect the functionality.

--- word_analysis_tests.py
+++ word_analysis_tests.py
@@ -1,13 +1,13 @@
 import unittest
 from typing import Tuple
 
-import verbs
+import word_analysis
 
 
 class VerbsTests(unittest.TestCase):
 
-    def __lcs_matrix_test_worker(self, word_a: str, word_b: str, expected: verbs.Matrix) -> None:
-        lcs = verbs.LCSMatrix(word_a, word_b)
+    def __lcs_matrix_test_worker(self, word_a: str, word_b: str, expected: word_analysis.Matrix) -> None:
+        lcs = word_analysis.LCSMatrix(word_a, word_b)
         self.assertEqual(lcs.word_a, word_a)
         self.assertEqual(lcs.word_b, word_b)
         self.assertEqual(lcs.matrix, expected)
@@ -68,8 +68,8 @@ class VerbsTests(unittest.TestCase):
                     (6, 5, 5, 4, 3, 2))
         self.__lcs_matrix_test_worker(word_a, word_b, expected)
 
-    def __lcs_intervals_test_worker(self, word_a: str, word_b: str, expected: Tuple[verbs.IntervalPair]) -> None:
-        intvs = verbs.WordSubsequenceIntervals(verbs.LCSMatrix(word_a, word_b))
+    def __lcs_intervals_test_worker(self, word_a: str, word_b: str, expected: Tuple[word_analysis.IntervalPair]) -> None:
+        intvs = word_analysis.WordSubsequenceIntervals(word_analysis.LCSMatrix(word_a, word_b))
         self.assertEqual(intvs.word_a, word_a)
         self.assertEqual(intvs.word_b, word_b)
         self.assertEqual(intvs.intervals, expected)
@@ -82,65 +82,65 @@ class VerbsTests(unittest.TestCase):
         # todo: is that a desirable output? probably not -> the above result is achieved with prioritizing edits or deletes over inserts
         # the lower result is achieved with prioritizing inserts in draws. is this always desirable? instead
         # of establishing an order, should the costs for steps in the lcs computation be adjusted?
-        expected_interval_pairs = (verbs.IntervalPair(verbs.Interval(0, 0), verbs.Interval(0, 2), False),
-                                   verbs.IntervalPair(verbs.Interval(0, 1), verbs.Interval(2, 3), True),
-                                   verbs.IntervalPair(verbs.Interval(1, 2), verbs.Interval(3, 3), False),
-                                   verbs.IntervalPair(verbs.Interval(2, 6), verbs.Interval(3, 7), True))
+        expected_interval_pairs = (word_analysis.IntervalPair(word_analysis.Interval(0, 0), word_analysis.Interval(0, 2), False),
+                                   word_analysis.IntervalPair(word_analysis.Interval(0, 1), word_analysis.Interval(2, 3), True),
+                                   word_analysis.IntervalPair(word_analysis.Interval(1, 2), word_analysis.Interval(3, 3), False),
+                                   word_analysis.IntervalPair(word_analysis.Interval(2, 6), word_analysis.Interval(3, 7), True))
         self.__lcs_intervals_test_worker(word_a, word_b, expected_interval_pairs)
 
     def test_build_intervals_halloh_hello(self) -> None:
         word_a = "halloh"
         word_b = "hello"
-        expected_interval_pairs = (verbs.IntervalPair(verbs.Interval(0, 1), verbs.Interval(0, 1), True),
-                                   verbs.IntervalPair(verbs.Interval(1, 2), verbs.Interval(1, 2), False),
-                                   verbs.IntervalPair(verbs.Interval(2, 5), verbs.Interval(2, 5), True),
-                                   verbs.IntervalPair(verbs.Interval(5, 6), verbs.Interval(5, 5), False))
+        expected_interval_pairs = (word_analysis.IntervalPair(word_analysis.Interval(0, 1), word_analysis.Interval(0, 1), True),
+                                   word_analysis.IntervalPair(word_analysis.Interval(1, 2), word_analysis.Interval(1, 2), False),
+                                   word_analysis.IntervalPair(word_analysis.Interval(2, 5), word_analysis.Interval(2, 5), True),
+                                   word_analysis.IntervalPair(word_analysis.Interval(5, 6), word_analysis.Interval(5, 5), False))
         self.__lcs_intervals_test_worker(word_a, word_b, expected_interval_pairs)
 
     def test_insert_transformation(self) -> None:
-        transf = verbs.InsertTransformation("bar")
+        transf = word_analysis.InsertTransformation("bar")
         transformed, transformee = transf.apply("foo", "2")
         self.assertEqual(transformed, "foobar")
         self.assertEqual(transformee, "2")
 
     def test_edit_transformation(self) -> None:
-        transf = verbs.EditTransformation("ugo")
+        transf = word_analysis.EditTransformation("ugo")
         transformed, transformee = transf.apply("h", "ilbert")
         self.assertEqual(transformed, "hugo")
         self.assertEqual(transformee, "ert")
 
     def test_delete_transformation(self) -> None:
-        transf = verbs.DeleteTransformation(2)
+        transf = word_analysis.DeleteTransformation(2)
         transformed, transformee = transf.apply("foo", "bar2")
         self.assertEqual(transformed, "foo")
         self.assertEqual(transformee, "r2")
 
     def test_skip_transformation(self) -> None:
-        transf = verbs.SkipTransformation(2)
+        transf = word_analysis.SkipTransformation(2)
         transformed, transformee = transf.apply("foo", "bar2")
         self.assertEqual(transformed, "fooba")
         self.assertEqual(transformee, "r2")
 
     def test_transformation_sequence(self) -> None:
-        transf = verbs.WordTransformationSequence(
-            [verbs.SkipTransformation(1),
-             verbs.DeleteTransformation(5),
-             verbs.SkipTransformation(1),
-             verbs.EditTransformation("o"),
-             verbs.InsertTransformation("bar")]
+        transf = word_analysis.WordTransformationSequence(
+            [word_analysis.SkipTransformation(1),
+             word_analysis.DeleteTransformation(5),
+             word_analysis.SkipTransformation(1),
+             word_analysis.EditTransformation("o"),
+             word_analysis.InsertTransformation("bar")]
         )
         transformed, transformee = transf.apply("", "function")
         self.assertEqual(transformed, "foobar")
         self.assertEqual(transformee, "")
 
     def test_build_word_transformation(self) -> None:
-        subsequence_intervals = verbs.WordSubsequenceIntervals(verbs.LCSMatrix("liegen", "gelegen"))
-        transf = verbs.build_word_transformation(subsequence_intervals)
-        expected = verbs.WordTransformationSequence(
-            [verbs.InsertTransformation("ge"),
-             verbs.SkipTransformation(1),
-             verbs.DeleteTransformation(1),
-             verbs.SkipTransformation(4)]
+        subsequence_intervals = word_analysis.WordSubsequenceIntervals(word_analysis.LCSMatrix("liegen", "gelegen"))
+        transf = word_analysis.build_word_transformation(subsequence_intervals)
+        expected = word_analysis.WordTransformationSequence(
+            [word_analysis.InsertTransformation("ge"),
+             word_analysis.SkipTransformation(1),
+             word_analysis.DeleteTransformation(1),
+             word_analysis.SkipTransformation(4)]
         )
         self.assertEqual(transf, expected)
         transformed, transformee = transf.apply("", "liegen")
