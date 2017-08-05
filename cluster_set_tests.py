157387b209e402e2eeb7b1ae2dd74b0d158f6aa5
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2017-08-05 19:07:03 +0200

    Added ClusterSet: A collection which allows grouping of equal elements in a single set into subsets (clusters).

---
+++ cluster_set_tests.py
@@ -0,0 +1,31 @@
+import unittest
+
+import cluster_set
+
+
+class ClusterSetTests(unittest.TestCase):
+
+    class TestElem:
+        def __init__(self, value, hash) -> None:
+            self._value = value
+            self._hash = hash
+
+        def __hash__(self) -> int:
+            return self._hash
+
+        def __eq__(self, other) -> bool:
+            return other._value == self._value
+
+        def __repr__(self) -> str:
+            return "(" + str(self._value) + " # " + str(self._hash) + ")@" + str(id(self))
+
+    def test_cluster_set(self) -> None:
+        c = cluster_set.ClusterSet()
+        c.add(self.TestElem(value=1, hash=1))
+        c.add(self.TestElem(value=2, hash=2))
+        c.add(self.TestElem(value=3, hash=2))
+        c.add(self.TestElem(value=2, hash=2))
+        expected = frozenset({frozenset({self.TestElem(value=1, hash=1)}),
+                              frozenset({self.TestElem(value=2, hash=2), self.TestElem(value=2, hash=2)}),
+                              frozenset({self.TestElem(value=3, hash=2)})})
+        self.assertEqual(c.get_clusters(), expected)
--- verbs.py
+++ verbs.py
@@ -1,31 +1,59 @@
 import sys
 import codecs
+from typing import TypeVar, Tuple, List
 #import tensorflow as tf
+#from sklearn.tree import DecisionTreeClassifier
 
 import word_analysis as ana
+from cluster_set import ClusterSet
+
+class TrainingSetElement:
+
+    def __init__(self, word_a: str, word_b: str) -> None:
+        self.__word_a = word_a
+        self.__word_b = word_b
+        self.__subsequence_intervals = ana.analyze_word_pair(self.__word_a, self.__word_b)
+
+    @property
+    def word_a(self) -> str:
+        return self.__word_a
+
+    @property
+    def word_b(self) -> str:
+        return self.__word_b
+
+    @property
+    def subsequence_intervals(self) -> ana.WordSubsequenceIntervals:
+        return self.__subsequence_intervals
+
+    def __hash__(self) -> int:
+        return hash(self.__subsequence_intervals)
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, TrainingSetElement): return False
+        return other.subsequence_intervals == self.subsequence_intervals
+
+    def __repr__(self) -> str:
+        return "(" + self.word_a + ", " + self.word_b + ", " + str(self.subsequence_intervals) + ")"
 
 print("Loading training word pairs...")
-word_pairs = []
+word_pairs = set()
 with codecs.open("words.txt", 'r', encoding='utf-8') as f:
     for line in f.readlines():
-        word_pairs.append([part.strip() for part in line.split(",")])
+        word_pairs.add(tuple(part.strip() for part in line.split(",")))
 
 
 print("Analyzing training word pairs...")
-training_set = [(pair[0], pair[1], ana.analyze_word_pair(pair[0], pair[1])) for pair in word_pairs]
+training_set = [TrainingSetElement(pair[0], pair[1]) for pair in word_pairs]
 
 print(str(training_set))
 
 print("Clustering training word pairs...")
-clusters = dict()
+clusters = ClusterSet()
 for elem in training_set:
-    id = hash(elem[2])
-    if id not in clusters:
-        clusters[id] = [elem]
-    else:
-        clusters[id].append(elem) # todo: needs equivalence checks and separate lists for non-equivalent elems with same hash
+    clusters.add(elem)
 
-print(clusters)
+print(clusters.get_clusters())
 
 # todo: make transformations invariant to skip lengths (same replacements and inserts -> covered by same transformation for different skip lengths)?
 # todo: use ML to discover similar rules for the above?
